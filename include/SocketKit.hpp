587eedb7807b377004cba3470577a63f5675c79a
eric3u <eric3u@outlook.com>
2019-01-07 23:57:53 +0800

    [Fix] 编码问题 [Fix] 重定义问题

--- include/SocketKit.hpp
+++ include/SocketKit.hpp
@@ -1,5 +1,4 @@
-#ifndef _SOCKET_KIT_HEADER_
-#define _SOCKET_KIT_HEADER_
+#pragma once
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -31,29 +30,7 @@ typedef int SocketFd;
 
 namespace socketkit
 {
-    bool initialize() {
-#if _WIN32
-        WSADATA wsaData;
-        int ret = ::WSAStartup(MAKEWORD(2, 2), &wsaData);
-        if (ret != 0) {
-            // error
-            perror("WSAStartup Error");
-            return false;
-        }
-#endif
-        return true;
-    };
-
-    bool relesase() {
-#if _WIN32
-        int ret = WSACleanup();
-        if (ret != 0) {
-            perror("WSACleanup Error");
-            return false;
-        }
-#endif
-        return true;
-    };
+    bool initialize();
+    bool release();
 
 }
-#endif // !_SOCKET_KIT_HEADER_
