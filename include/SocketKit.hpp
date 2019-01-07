75466ff96c96c5243a21706cc56ba25fe49ee8f1
eric3u <eric3u@outlook.com>
2019-01-07 23:36:34 +0800

    [Refector] 命名空间修改 [Fix] utf8编码

--- include/SocketKit.hpp
+++ include/SocketKit.hpp
@@ -1,9 +1,33 @@
-#pragma once
+#ifndef _SOCKET_KIT_HEADER_
+#define _SOCKET_KIT_HEADER_
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <iostream>
 
 #if _WIN32
 #include <WinSock2.h>
+typedef struct sockaddr_in SOCKADDR_IN;
+typedef struct sockaddr SOCKADDR;
+typedef SOCKET SocketFd;
+typedef int ssize_t;
+typedef int socklen_t;
+#define SHUT_RDWR SD_BOTH
 #endif // _WIN32
 
+#if __linux__
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <netinet/tcp.h>
+#include <sys/fcntl.h>
+#include <sys/select.h>
+
+typedef int SocketFd;
+#endif
 
 namespace socketkit
 {
@@ -30,4 +54,6 @@ namespace socketkit
 #endif
         return true;
     };
+
 }
+#endif // !_SOCKET_KIT_HEADER_
