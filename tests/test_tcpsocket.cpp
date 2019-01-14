dbbbc5ef60ef7cf555fcc32cf0aecea002177279
eric3u <eric3u@outlook.com>
2019-01-14 15:45:08 +0800

    [Test] 重构后更新Demo测试用例（ECHO）

--- tests/test_tcpsocket.cpp
+++ tests/test_tcpsocket.cpp
@@ -2,23 +2,24 @@
 #include <thread>
 #include <chrono>
 #include <string>
-#include "TCPConnection.hpp"
+
+#include "TCPSocket.hpp"
 
 
 class MyClass final : socketkit::utils::NoCopyable {
 private:
-    std::shared_ptr<socketkit::TCPConnection> _connection;
+    std::shared_ptr<socketkit::TCPSocket> _connection;
     uchar buffer[4096] = {0};
 public:
-    void eventCallback(socketkit::CommunicatorEvent event) {
+    void eventCallback(socketkit::ICommunicator *communicator, socketkit::CommunicatorEvent event) {
         switch (event) {
             case socketkit::CommunicatorEvent::HasBytesAvailable : {
                 printf("can read\n");
                 int s = 4096;
                 _connection->read([this](uchar *data, int size){
-                    using namespace std;
-                    string s((char *)data, size);
-                    cout<<s<<endl;
+                    if (size > 0) {
+                        _connection->write(data, size);
+                    }
                 });
 
                 break;
@@ -42,14 +43,12 @@ public:
     }
 
     MyClass() {
-        _connection= std::make_shared<socketkit::TCPConnection>();
+        _connection= std::make_shared<socketkit::TCPSocket>();
         auto endpoint = std::make_shared<socketkit::Endpoint>("127.0.0.1", 12002);
-        auto callback = std::bind(&MyClass::eventCallback, this, std::placeholders::_1);
+        auto callback = std::bind(&MyClass::eventCallback, this, std::placeholders::_1, std::placeholders::_2);
         _connection->mEventHandler = callback;
         _connection->getRunloop()->run();
-
         _connection->connect(endpoint);
-
     }
 
 };
