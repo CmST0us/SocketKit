11301ad5d07c3a2637f40fe750e52928b3c3f19c
eric3u <eric3u@outlook.com>
2019-01-17 13:26:32 +0800

    [Fix] 添加data扩展空间时重分配指针断言检查

---
+++ include/libsocketkit/data.hpp
@@ -0,0 +1,67 @@
+//
+// Created by CmST0us on 2019/1/15.
+//
+
+#pragma once
+
+#include "NoCopyable.hpp"
+
+namespace socketkit {
+namespace utils {
+
+class Data final : public NoCopyable {
+public:
+    Data(void *ptr, int size) : _ptr{ptr}, _size{size} {
+        clear();
+    }
+
+    Data(int size) : _size{size} {
+        _ptr = (void *)new unsigned char[size];
+        memset(_ptr, 0, size);
+    }
+
+    void *getDataAddress() {
+        return _ptr;
+    }
+
+    const int getDataSize() const {
+        return _size;
+    }
+
+    bool copy(const void *src, int size) {
+        if (size > _size) {
+            return false;
+        }
+        ::memcpy(_ptr, src, size);
+        return true;
+    }
+
+    void clear() {
+        memset(_ptr, 0, _size);
+    }
+
+    void expansion(int newSize) {
+        if (newSize > _size) {
+            void *rePtr = ::realloc(_ptr, newSize);
+            assert(rePtr != NULL);
+            _ptr = rePtr;
+            _size = newSize;
+        }
+        clear();
+    }
+
+
+    ~Data() {
+        if (_ptr != nullptr) {
+            free(_ptr);
+        }
+        _ptr = nullptr;
+        _size = 0;
+    }
+private:
+    void *_ptr{nullptr};
+    int _size{0};
+};
+
+};
+};
