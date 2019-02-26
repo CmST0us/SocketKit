7811cde0aa42f01e8b88cfce7f08b83e42113658
eric3u <eric3u@outlook.com>
2019-02-27 01:51:40 +0800

    [Chore] 确保build目录存在

--- tools/build_libplist.sh
+++ tools/build_libplist.sh
@@ -3,6 +3,7 @@ CURRENT_DIR=`pwd`
 VENDOR_DIR=$CURRENT_DIR/../vendor
 BUILD_DIR=$VENDOR_DIR/build
 
+mkdir -p $BUILD_DIR
 cd $VENDOR_DIR/libplist
 ./autogen.sh --prefix=$BUILD_DIR --without-cython
 make
