fcb9c3297874998dcfc28c37aa1f38fa8c38303c
eric3u <eric3u@outlook.com>
2019-02-27 02:05:20 +0800

    [Chore] ios SocketKit 编译脚本修改

--- tools/setup_ios.sh
+++ tools/setup_ios.sh
@@ -3,13 +3,8 @@ CURRENT_DIR=`pwd`
 SOURCE_ROOT=$CURRENT_DIR/..
 VENDER_ROOT=$SOURCE_ROOT/..
 
+cd $SOURCE_ROOT
 mkdir -p $VENDER_ROOT/build/include
 mkdir -p build
 cd build
 cmake -GXcode -DBUILD_FOR_IOS=ON ..
-xcodebuild
-xcodebuild -target install
-
-rm -r $SOURCE_ROOT/../build/include/libSocketKit
-mv libSocketKit/include/libSocketKit $SOURCE_ROOT/../build/include/
-
