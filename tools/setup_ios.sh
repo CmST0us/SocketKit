fc45ff5a0a73a0c5ff87ba27531a52145cb6d621
eric3u <eric3u@outlook.com>
2019-03-07 12:39:34 +0800

    [Chore] ios 构建脚本当前目录变量为脚本所在位置

--- tools/setup_ios.sh
+++ tools/setup_ios.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-CURRENT_DIR=`pwd`
+CURRENT_DIR=$(cd "$(dirname "${BASH_SOURCE-$0}")"; pwd)
 SOURCE_ROOT=$CURRENT_DIR/..
 VENDER_ROOT=$SOURCE_ROOT/..
 
 cd $SOURCE_ROOT
 mkdir -p $VENDER_ROOT/build/include
+rm -r build
 mkdir -p build
 cd build
 cmake -GXcode -DBUILD_FOR_IOS=ON ..
+
